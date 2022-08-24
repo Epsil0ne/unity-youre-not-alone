8a867622fc680b1775b640bb267c9d85caedc5da
Epsil0ne <35867069+Epsil0ne@users.noreply.github.com>
2022-08-24 19:42:59 +0200

    Add 3rd person movements & camera + sample scene

---
+++ Youre not alone/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadGame() {
+        SceneManager.LoadScene(1);
+    }
+}
