8ae7825a4ff58571d0630ecdb3d483b292bfd4ac
Diego Lima <gmdiegolima@hotmail.com>
2022-09-04 13:33:12 -0300

    Screenshot script, post-processing and a little bit more destructive objects

---
+++ Youre not alone/Assets/Screenshot.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKeyDown("p")){ // capture screen shot on left mouse button down
+
+            string folderPath = "Assets/Screenshots/"; // the path of your project folder
+
+            if (!System.IO.Directory.Exists(folderPath)) // if this path does not exist yet
+                System.IO.Directory.CreateDirectory(folderPath);  // it will get created
+            
+            var screenshotName =
+                                    "Screenshot_" +
+                                    System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss") + // puts the current time right into the screenshot name
+                                    ".png"; // put youre favorite data format here
+            ScreenCapture.CaptureScreenshot(System.IO.Path.Combine(folderPath, screenshotName),2); // takes the sceenshot, the "2" is for the scaled resolution, you can put this to 600 but it will take really long to scale the image up
+            Debug.Log(folderPath + screenshotName); // You get instant feedback in the console
+        }
+    }
+}
